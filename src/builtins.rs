a55b6adb57c99f5ffe9552b07a66c897c2ddc089
Edgar Manukyan <armenic@gmail.com>
2023-08-06 22:19:23 -0400

    add sep param

--- src/builtins.rs
+++ src/builtins.rs
@@ -782,8 +782,23 @@ pub fn primitive_paste(args: ExprList, env: &mut Environment) -> EvalResult {
     // TODO
     // Support lists
 
-    let R::List(vals) = env.eval_list(args)? else {
+    // Need the sep and collapse parameter
+    let sep_i = &args.keys.iter().position(|k| k == &Some("sep".to_string()));
+
+    let R::List(mut vals) = env.eval_list(args)? else {
         unreachable!()
+    };
+
+    let sep_c = match sep_i {
+        Some(i) => {
+            let sep_val = vals.remove(*i);
+            match sep_val {
+                (_, R::Vector(Vector::Character(s_v))) => s_v.get(0).unwrap().clone(),
+                _ => unreachable!(),
+            }
+        }
+        // Default value for sep parameter is a space
+        _ => OptionNA::Some(" ".to_string()),
     };
 
     let vals: Vec<_> = vals
@@ -794,7 +809,7 @@ pub fn primitive_paste(args: ExprList, env: &mut Environment) -> EvalResult {
     // Coerce everything into strings
     let char_vals: Vec<R> = vals
         .iter()
-        .map(|x| x.clone().1.as_character().unwrap())
+        .map(|v| v.clone().1.as_character().unwrap())
         .collect();
 
     let vec_of_vectors: Vec<_> = char_vals
@@ -815,9 +830,19 @@ pub fn primitive_paste(args: ExprList, env: &mut Environment) -> EvalResult {
             // Any shorter vector will re-cycle its values to the length of
             // longest one
             .zip(vec_of_vectors[i].iter().cycle())
-            .map(|(x, y)| format!("{}{}", x, y))
+            .map(|(x, y)| format!("{x}{sep_c}{y}"))
             .collect();
     }
+
+    iter = iter
+        .iter()
+        .map(|x| {
+            let mut new_x = x.clone();
+            // Need to remove the sep character from the beginning
+            new_x.remove(0);
+            new_x
+        })
+        .collect::<Vec<String>>();
 
     Ok(R::Vector(iter.into()))
 }
@@ -827,7 +852,42 @@ mod test_primitive_paste {
     use super::*;
 
     #[test]
-    fn test_primitive_paste() {
+    fn test_primitive_paste_01() {
+        let mut env = Environment::default();
+
+        // Making a value of args parameter of primitive_paste corresponding to R
+        // c(1.1, 2, FALSE, "a", c(1, 2), sep = "+")
+        let args = ExprList {
+            keys: vec![None, None, None, None, None, None, Some("sep".to_string())],
+            values: vec![
+                Expr::Number(1.1),
+                Expr::Null,
+                Expr::Integer(2),
+                Expr::Bool(false),
+                Expr::String("a".to_string()),
+                Expr::Call(
+                    Box::new(Expr::Symbol("c".to_string())),
+                    ExprList {
+                        keys: vec![None; 2],
+                        values: vec![Expr::Number(1.0), Expr::Number(2.0)],
+                    },
+                ),
+                // sep parameter
+                Expr::String("+".to_string()),
+            ],
+        };
+
+        let _observed = primitive_paste(args, &mut env).unwrap().get_vec_string();
+        let _expected: Vec<_> = vec!["1.1+2+false+a+1", "1.1+2+false+a+2"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+
+        assert_eq!(_observed, _expected);
+    }
+
+    #[test]
+    fn test_primitive_paste_02() {
         let mut env = Environment::default();
 
         // Making a value of args parameter of primitive_paste corresponding to R
@@ -851,7 +911,7 @@ mod test_primitive_paste {
         };
 
         let _observed = primitive_paste(args, &mut env).unwrap().get_vec_string();
-        let _expected: Vec<_> = vec!["1.12falsea1", "1.12falsea2"]
+        let _expected: Vec<_> = vec!["1.1 2 false a 1", "1.1 2 false a 2"]
             .iter()
             .map(|s| s.to_string())
             .collect();
