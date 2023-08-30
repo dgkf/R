d3e666204891e3a7a82c5819c0b8561c528e51e0
dgkf <18220321+dgkf@users.noreply.github.com>
2023-08-30 03:01:27 +0000

    Deploying to gh-pages from @ dgkf/R@df4b9a2caa6f808ae1702f34e95dd7695c5fcb52 🚀

--- pkg/r.js
+++ pkg/r.js
@@ -234,8 +234,8 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper240 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 148, __wbg_adapter_8);
+    imports.wbg.__wbindgen_closure_wrapper235 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 145, __wbg_adapter_8);
         return addHeapObject(ret);
     };
 
