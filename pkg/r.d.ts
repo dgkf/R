518fdbd673b06bdf0cbcc458a82e7ac909e0ca3f
dgkf <dgkf@users.noreply.github.com>
2024-03-01 15:19:50 +0000

    Deploying to gh-pages from @ dgkf/R@f7c40b2a76a38becd9a5f3f9b1f799973a7018ea 🚀

--- pkg/r.d.ts
+++ pkg/r.d.ts
@@ -34,18 +34,18 @@ export function wasm_parse_errors(args: any, input: string): (ParseError)[];
 export function wasm_highlight(args: any, input: string): any[];
 /**
 */
-export enum Experiment {
-  TailCalls = 0,
-  RestArgs = 1,
-}
-/**
-*/
 export enum Localization {
   En = 0,
   Es = 1,
   Cn = 2,
   Pirate = 3,
   Emoji = 4,
+}
+/**
+*/
+export enum Experiment {
+  TailCalls = 0,
+  RestArgs = 1,
 }
 /**
 * Run the R REPL
