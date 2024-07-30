73df84776409da00f1818949b7ff2e27b58b8d32
Doug Kelkhoff <18220321+dgkf@users.noreply.github.com>
2024-07-30 15:05:04 -0400

    Update src/repl/release.rs

--- src/repl/release.rs
+++ src/repl/release.rs
@@ -36,7 +36,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.");
 }
 
-pub const AVAILABLE_FUNCTIONS: &str = "\nSee a list of implemented functions using `names(parent())`\n";
+pub const AVAILABLE_FUNCTIONS: &str =
+    "\nSee a list of implemented functions using `names(parent())`\n";
 
 #[allow(clippy::const_is_empty)]
 pub fn session_header(session: &Session) -> String {
